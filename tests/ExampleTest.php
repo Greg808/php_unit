96af51bedb537593a9272e5fb440da906ece193e
Gregor Weindl <g.weindl@pixelpoems.com>
2021-04-20 16:49:22 +0200

    the most basic unit test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,12 +1,24 @@
 <?php
+/**
+ * the most basic unit test
+ */
 
 
 use PHPUnit\Framework\TestCase;
-
+/**
+ * create test class extend PHPUnit\Framework\TestCase
+ */
 class ExampleTest extends TestCase
 {
+    /**
+     * public function that starts with test
+     * and describes what the test does
+     */
     public function testAddingTwoPlusTwoIsFour()
     {
+        /**
+         * add assertion
+         */
         self::assertEquals(4, 2 + 2);
     }
 }
