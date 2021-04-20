8cc570ec36af77c080ea08dbcce26174f690d309
Gregor Weindl <g.weindl@pixelpoems.com>
2021-04-20 17:19:27 +0200

    multiple test function

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -7,11 +7,27 @@ use PHPUnit\Framework\TestCase;
 
 class FunctionTest extends TestCase
 {
+    /**
+     * A Test class can have multiple test function
+     */
     public function testAddReturnsTheCorrectSum(): void
     {
         /** require the functions file */
         require 'functions.php';
         /** test the add function  */
-        self::assertEquals(4,add(2,2));
+        self::assertEquals(4, add(2, 2));
+        /**
+         * multiple assertions can be run in the same
+         * test function if it makes sense
+         */
+        self::assertEquals(8, add(5, 3));
+    }
+
+    public function testAddDoesNotReturnTheCorrectSum(): void
+    {
+        /**
+         * Testing that the add method does not return a false value
+         */
+        self::assertNotEquals(5, add(2, 2));
     }
 }
