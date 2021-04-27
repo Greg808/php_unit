55b56f7a0098534e67f98f6b136c043100bea734
Gregor Weindl <g.weindl@pixelpoems.com>
2021-04-27 16:07:17 +0200

    tests for user class

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -5,6 +5,7 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+
     public function testReturnsFullName(): void
     {
         $user = new User();
@@ -17,5 +18,40 @@ class UserTest extends TestCase
     {
         $user = new User();
         self::assertEquals('', $user->getFullName());
+    }
+
+    public function testNotificationSent(): void
+    {
+        $user = new User();
+
+        $mock_mailer = $this->createMock(Mailer::class);
+
+        $mock_mailer->expects(self::once())
+            ->method('sendMessage')
+            ->with(self::equalTo('foo@bar.at'), self::equalTo('awesome'))
+            ->willReturn(true);
+
+        $user->setMailer($mock_mailer);
+
+        $user->email = 'foo@bar.at';
+
+        self::assertTrue($user->notify('awesome'));
+    }
+
+    public function testNotificationCanNotBeSendWithOutEmail(): void
+    {
+        $user = new User();
+
+        $mock_mailer = $this->getMockBuilder(Mailer::class)
+            ->onlyMethods(['sendMessage'])
+            ->getMock();
+
+        $user->setMailer($mock_mailer);
+
+        $this->expectException(Exception::class);
+
+        $this->expectExceptionMessage('no email provided');
+
+        $user->notify('awesome');
     }
 }
