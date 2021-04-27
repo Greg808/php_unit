d2fc76914ebea0d2d27c44689f23b401c5708bf0
Gregor Weindl <g.weindl@pixelpoems.com>
2021-04-27 16:06:17 +0200

    new classes to test

--- src/User.php
+++ src/User.php
@@ -7,12 +7,33 @@ class User
 
     public ?string $firstName = null;
 
+    public ?string $email = null;
+
+    protected Mailer $mailer;
+
     public function getFullName(): string
     {
         /**
          * before return "$this->firstName $this->lastName"
          */
         return trim("$this->firstName $this->lastName");
+    }
+
+    public function notify(string $message): bool
+    {
+        if (!$this->email) {
+            throw new Exception('no email provided');
+        }
+        $mailer = $this->mailer;
+        return $mailer->sendMessage($this->email, $message);
+    }
+
+    /**
+     * @param Mailer $mailer
+     */
+    public function setMailer(Mailer $mailer): void
+    {
+        $this->mailer = $mailer;
     }
 
 }
